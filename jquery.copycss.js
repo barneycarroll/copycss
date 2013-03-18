1559ad749832e1bb51ed60d861db91e53a8aa458
Barney Carroll <barney.carroll@gmail.com>
2013-03-18 01:26:08 +0000

    Iterative process of moving function definitions outside of main method call

--- jquery.copycss.js
+++ jquery.copycss.js
@@ -21,44 +21,39 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 (function($){
-	
+
+	// convenience methods to turn css case ('background-image') to camel ('backgroundImage')
+	var camelize = function(string){
+		return string.replace(/\-([a-z])/g, function(a, b){
+			return b.toUpperCase();
+		});
+	};
+
 	$.fn.getStyles = function(only, except){
-		
 		// the map to return with requested styles and values as KVP
 		var product = {};
-		
+
 		// the style object from the DOM element we need to iterate through
 		var style;
-		
+
 		// recycle the name of the style attribute
 		var name;
-		
+
 		// if it's a limited list, no need to run through the entire style object
 		if(only && only instanceof Array){
-			
 			for(var i = 0, l = only.length; i < l; i++){
 				// since we have the name already, just return via built-in .css method
-				name = only[i];
+				name          = only[i];
 				product[name] = this.css(name);
 			}
-			
+
 		} else {
-			
+
 			// otherwise, we need to get everything
 			var dom = this.get(0);
-			
+
 			// standards
 			if (window.getComputedStyle) {
-				
-				// convenience methods to turn css case ('background-image') to camel ('backgroundImage')
-				var pattern = /\-([a-z])/g;
-				var uc = function (a, b) {
-						return b.toUpperCase();
-				};			
-				var camelize = function(string){
-					return string.replace(pattern, uc);
-				};
-				
 				// make sure we're getting a good reference
 				if (style = window.getComputedStyle(dom, null)) {
 					var camel, value;
@@ -93,9 +88,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 					}
 				}
 			}
-			
+
 		}
-		
+
 		// remove any styles specified...
 		// be careful on blacklist - sometimes vendor-specific values aren't obvious but will be visible...  e.g., excepting 'color' will still let '-webkit-text-fill-color' through, which will in fact color the text
 		if(except && except instanceof Array){
@@ -104,16 +99,16 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 				delete product[name];
 			}
 		}
-		
+
 		// one way out so we can process blacklist in one spot
 		return product;
-	
+
 	};
-	
+
 	// sugar - source is the selector, dom element or jQuery instance to copy from - only and except are optional
 	$.fn.copyCSS = function(source, only, except){
 		var styles = $(source).getStyles(only, except);
 		this.css(styles);
 	};
-	
+
 })(jQuery);
